241d5b5db541f175cc1054518c69b1a6b5436c90
Ultrabenosaurus <naebeth@googlemail.com>
2015-02-06 15:26:33 +0000

    minified stuff for no real reason

--- devtools.js
+++ devtools.js
@@ -1,12 +1,6 @@
 var x = new XMLHttpRequest();
-x.open('GET', 'Custom.css');
+x.open('GET', 'Custom.min.css');
 x.onload = function() {
     chrome.devtools.panels.applyStyleSheet(x.responseText);
 };
 x.send();
-
-cmContainer = document.querySelector( "div.CodeMirror.cm-s-default.source-code.fill" );
-if( cmContainer )
-{
-    cmContainer.className = cmContainer.className.replace( "cm-s-default", "cm-s-themed" );
-}
